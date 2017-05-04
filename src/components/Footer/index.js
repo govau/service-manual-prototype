34fcefa41d09338d6e3c9652a95741704a85f105
Simon Schwartz <simon.schwartz0@gmail.com>
2017-05-04 12:33:57 +1000

    make service manual work offline

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ const Footer = () => (
   <footer className="uikit-footer" role="contentinfo">
     <div className="container">
       <section className="uikit-footer__end">
-        <img className="uikit-responsive-media-img" src="assets/coat-of-arms.png" height="100" alt="Commonwealth Coat of Arms crest logo"></img>
+        <img className="uikit-responsive-media-img" src="/assets/coat-of-arms.png" height="100" alt="Commonwealth Coat of Arms crest logo"></img>
         <p>
           <small>© Commonwealth of Australia</small>
         </p>
